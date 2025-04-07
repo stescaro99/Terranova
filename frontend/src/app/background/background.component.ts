f712bc54c5d87cd8066ae320f6fe5b0df8494f74
fgori <98gori@gmail.com>
2025-04-07 13:09:52 +0200

    background

--- frontend/src/app/background/background.component.ts
+++ frontend/src/app/background/background.component.ts
@@ -7,46 +7,38 @@ import { CommonModule } from '@angular/common';
   templateUrl: './background.component.html',
   styleUrl: './background.component.scss'
 })
+
 export class BackgroundComponent {
-  cubes: { top: string; left: string; borderRadius: string, animationDelay: string  }[] = [];
+  images: { src: string; top: string; left: string; animationDelay: string; size: string; rotation: string }[] = [];
+  isPopupVisible: boolean = false; // Proprietà per controllare la visibilità del pop-up
 
   constructor() {
-    this.generateNonOverlappingCubes(20); 
+    this.generateImages(20); // Genera 10 immagini dinamiche
   }
 
-  generateNonOverlappingCubes(count: number) {
-    const cubeSize = 70; 
-    const margin = 10;
-    const positions: { top: number; left: number }[] = [];
+  generateImages(count: number) {
+    const imagePaths = [
+      'assets/background/ice.png',
+      'assets/background/ice_two.png',
+      'assets/background/ice_boar.png',
+      'assets/background/image_tree.png',
+      'assets/background/mint.png',
+      'assets/background/orange.png',
+    ];
   
     for (let i = 0; i < count; i++) {
-      let top: number;
-      let left: number;
-      let isOverlapping: boolean;
+      const randomImage = imagePaths[Math.floor(Math.random() * imagePaths.length)];
+      const top = '100vh'; // Inizia sempre fuori dal fondo dello schermo
+      const left = Math.random() * 100 + '%'; // Posizione orizzontale casuale
+      const animationDelay = Math.random() * 10 + 's'; // Ritardo casuale fino a 10 secondi
+      const size = Math.random() * 100 + 50 + 'px'; // Dimensione casuale tra 50px e 150px
+      const rotation = Math.random() * 360 + 'deg'; // Rotazione casuale tra 0° e 360°
   
-      do {
-        isOverlapping = false;
-        top = Math.random() * (window.innerHeight - cubeSize);
-        left = Math.random() * (window.innerWidth - cubeSize);
-  
-        for (const pos of positions) {
-          const distance = Math.sqrt(
-            Math.pow(pos.top - top, 2) + Math.pow(pos.left - left, 2)
-          );
-          if (distance < cubeSize + margin) {
-            isOverlapping = true;
-            break;
-          }
-        }
-      } while (isOverlapping);
-  
-      positions.push({ top, left });
-      this.cubes.push({
-        top: `${top}px`,
-        left: `${left}px`,
-        borderRadius: `${Math.random() * 30}px`,
-        animationDelay: `${Math.random() * 5}s`, // Ritardo casuale per l'animazione
-      });
+      this.images.push({ src: randomImage, top, left, animationDelay, size, rotation });
     }
+  }
+
+  togglePopup() {
+    this.isPopupVisible = !this.isPopupVisible; // Alterna la visibilità del pop-up
   }
 }
