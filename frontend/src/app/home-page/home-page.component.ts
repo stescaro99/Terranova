b676e309d848897d9c8a4f9a208222614d219796
fgori <98gori@gmail.com>
2025-04-08 14:02:45 +0200

    cocktail

--- frontend/src/app/home-page/home-page.component.ts
+++ frontend/src/app/home-page/home-page.component.ts
@@ -5,6 +5,8 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { BackgroundComponent } from '../background/background.component';
 import { CocktailService } from '../services/cocktail.service';
+import { Cocktail } from '../models/cocktail';
+import { error } from 'console';
 
 @Component({
   selector: 'app-home-page',
@@ -16,7 +18,8 @@ export class HomePageComponent {
   user: User;
   isAuthenticated: boolean;
   isDropdownOpen: boolean = false;
-
+  cocktail: Cocktail | null = null;
+  
   constructor(private userService: UserService, private router: Router, private cocktailService: CocktailService) {
     this.user = this.userService.getUser();
     console.log('Valore di user.ImgUrl:', this.user.ImgUrl);
@@ -28,12 +31,16 @@ export class HomePageComponent {
 
   takeCocktail() {
     this.cocktailService.takeCocktailOfDay().subscribe(
-      (response: any) => {
-        if(response)
-        {
-
+      (response: Cocktail) => {
+        if(response) {
+          console.log('Cocktail del giorno:', response); 
+          this.cocktail = response;
         }
-      });
+      },
+    (error) => {
+        console.error('Errore durante il recupero del cocktail del giorno:', error);
+      }
+    );
   }
 
   onUserPhotoClick() {
--- frontend/src/app/models/cocktail.ts
+++ frontend/src/app/models/cocktail.ts
@@ -1,11 +1,71 @@
-export interface Cocktail {
-    id: number; 
-    name: string; 
-    imageUrl: string;
-    ingredients: string[]; 
-    instructions: string; 
-    glassType: string; 
-    category: string; 
-    alcoholic: boolean;
-    createdBy: string;
-}
+export class Cocktail {
+    id: number;
+    drink: CocktailApiDrink;
+    createdByUserId?: number;
+    favoriteByUsers: string[]; // Array di ID o nomi degli utenti che hanno aggiunto il cocktail ai preferiti
+  
+    constructor(init?: Partial<Cocktail>) {
+      this.id = init?.id || 0;
+      this.drink = init?.drink || new CocktailApiDrink();
+      this.createdByUserId = init?.createdByUserId;
+      this.favoriteByUsers = init?.favoriteByUsers || [];
+    }
+  }
+  
+  export class CocktailApiDrink {
+    idDrink?: string;
+    strDrink?: string;
+    strDrinkAlternate?: string;
+    strTags?: string;
+    strVideo?: string;
+    strCategory?: string;
+    strIBA?: string;
+    strAlcoholic?: string;
+    strGlass?: string;
+    strInstructions?: string;
+    strInstructionsES?: string;
+    strInstructionsDE?: string;
+    strInstructionsFR?: string;
+    strInstructionsIT?: string;
+    strInstructionsZH_HANS?: string;
+    strInstructionsZH_HANT?: string;
+    strDrinkThumb?: string;
+    strIngredient1?: string;
+    strIngredient2?: string;
+    strIngredient3?: string;
+    strIngredient4?: string;
+    strIngredient5?: string;
+    strIngredient6?: string;
+    strIngredient7?: string;
+    strIngredient8?: string;
+    strIngredient9?: string;
+    strIngredient10?: string;
+    strIngredient11?: string;
+    strIngredient12?: string;
+    strIngredient13?: string;
+    strIngredient14?: string;
+    strIngredient15?: string;
+    strMeasure1?: string;
+    strMeasure2?: string;
+    strMeasure3?: string;
+    strMeasure4?: string;
+    strMeasure5?: string;
+    strMeasure6?: string;
+    strMeasure7?: string;
+    strMeasure8?: string;
+    strMeasure9?: string;
+    strMeasure10?: string;
+    strMeasure11?: string;
+    strMeasure12?: string;
+    strMeasure13?: string;
+    strMeasure14?: string;
+    strMeasure15?: string;
+    strImageSource?: string;
+    strImageAttribution?: string;
+    strCreativeCommonsConfirmed?: string;
+    dateModified?: string;
+  
+    constructor(init?: Partial<CocktailApiDrink>) {
+      Object.assign(this, init);
+    }
+  }
