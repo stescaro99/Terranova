279bf9bceaef876cdde5c841c4e8e35102f65c26
stescaro99 <stescaro@student.42firenze.it>
2025-05-12 12:09:02 +0200

    starters

--- start.sh
+++ start.sh
@@ -6,4 +6,10 @@ if [ ! -d "./LibreTranslate" ]; then
 fi
 
 echo "Starting Docker Compose..."
-docker-compose up --build
+docker-compose up --build -d
+
+echo "Waiting for services to start... (the first time may take a couple of minutes after the build)"
+sleep 10
+
+echo "Opening localhost:4200 in the default browser..."
+xdg-open http://localhost:4200 || open http://localhost:4200 || start http://localhost:4200
